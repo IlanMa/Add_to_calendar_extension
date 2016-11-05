9be1ee300f42f5e91d1bc95a993d49d313aa26ff
ilanMa <ilan.mamontov@gmail.com>
2016-11-05 15:45:06 -0400

    Inserting to google calendar successful

--- background.js
+++ background.js
@@ -1,26 +1,19 @@
-// chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-//     // Use the token.
-//     console.log(token);
-//     chrome.identity.removeCachedAuthToken({token: token}, function(info) {
-//     	console.log("?", info)
-//     })
-// });
-
-
-
-
 chrome.identity.getAuthToken({
     interactive: true
 }, function(token) {
+
     if (chrome.runtime.lastError) {
         alert(chrome.runtime.lastError.message);
         return;
     }
     var x = new XMLHttpRequest();
-    console.log(token)
-    x.open('GET', 'https://www.googleapis.com/calendar/v3/calendars?alt=json&access_token=' + token);
-    x.onload = function() {
-        alert(x.response);
-    };
-    x.send();
+    x.onreadystatechange = function() { //Call a function when the state changes.
+        if (x.readyState == 4 && x.status == 200) {
+            alert(x.responseText);
+        }
+    }
+    x.open('POST', 'https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=' + token, true);
+    x.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var event = { "data": {"title": "AAA","when": [{"start": "2010-04-17T15:00:00.000Z","end": "2010-04-17T17:00:00.000Z"}]}};
+    x.send({body: event});
 });
